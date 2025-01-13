e98b440d514f5f9fa151493bae556eaaa2db74ff
Nguyen Tuan Thanh <thanhpro06081999@gmail.com>
2025-01-13 23:54:18 +0700

    commit

--- app/src/main/java/com/files/video/downloader/videoplayerdownloader/downloader/ui/processing/ProcessAdapter.kt
+++ app/src/main/java/com/files/video/downloader/videoplayerdownloader/downloader/ui/processing/ProcessAdapter.kt
@@ -1,16 +1,123 @@
 package com.files.video.downloader.videoplayerdownloader.downloader.ui.processing
 
+import android.content.Context
+import android.graphics.Color
+import android.util.DisplayMetrics
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.files.video.downloader.videoplayerdownloader.downloader.R
 import com.files.video.downloader.videoplayerdownloader.downloader.data.network.entity.ProgressInfo
+import com.files.video.downloader.videoplayerdownloader.downloader.databinding.ItemProgressBinding
+import com.google.android.material.color.MaterialColors
 
-class ProcessAdapter(private var progressInfos: List<ProgressInfo>,
-                     private var videoListener: ProgressListener) : RecyclerView.Adapter<ProcessAdapter.ViewHolder>(){
-    class ViewHolder {
+class ProcessAdapter(
+    private var progressInfos: List<ProgressInfo>,
+    private var videoListener: ProgressListener
+) : RecyclerView.Adapter<ProcessAdapter.ViewHolder>() {
+    inner class ViewHolder(val binding: ItemProgressBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(progressInfo: ProgressInfo, position: Int) {
+            val thumbnail = progressInfo.videoInfo.thumbnail
+            val placeholder = R.drawable.ic_play_download
+            val size = getScreenResolution(itemView.context)
+            val color =
+                MaterialColors.getColor(itemView.context, R.attr.colorSurfaceVariant, Color.YELLOW)
 
+            with(binding)
+            {
+                this.cardProgress.setCardBackgroundColor(color)
+
+                var isPlay = true
+
+                val downloadId = progressInfo.downloadId
+                val isRegular = progressInfo.videoInfo.isRegularDownload
+
+                Glide.with(this@ViewHolder.itemView.context).load(thumbnail).fitCenter()
+                    .error(placeholder)
+                    .placeholder(placeholder)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .apply(RequestOptions().override(size.first / 8, size.second / 8))
+                    .into(this.ivThumbnail)
+
+                tvTitle.text = progressInfo.videoInfo.name
+                progressBar.progress = progressInfo.progress
+                tvProgress.text = progressInfo.progressSize
+                infoLine.text = progressInfo.infoLine
+                imgClose.setOnClickListener {
+                    videoListener.onCloseClicked(downloadId, isRegular)
+                }
+
+                if(progressInfo.infoLine.isEmpty()){
+                    isPlay = false
+                    imgPlayPause.setImageResource(R.drawable.ic_play_download)
+                }else{
+                    isPlay = true
+                    imgPlayPause.setImageResource(R.drawable.ic_pause_download)
+                }
+
+                imgPlayPause.setOnClickListener {
+                    videoListener.onPlayPauseDownloadClicked(
+                        this@ViewHolder.itemView,
+                        downloadId,
+                        isRegular,
+                        isPlay
+                    )
+                }
+
+            }
+
+        }
+
+        private fun getScreenResolution(context: Context): Pair<Int, Int> {
+            val displayMetrics = DisplayMetrics()
+            val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+
+            windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+            val widthPixels = displayMetrics.widthPixels
+            val heightPixels = displayMetrics.heightPixels
+
+            return Pair(widthPixels, heightPixels)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemProgressBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return progressInfos.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(progressInfos[position], position)
+    }
+
+    fun setData(progressInfos: List<ProgressInfo>) {
+        this.progressInfos = progressInfos
+        notifyDataSetChanged()
     }
 }
 
 interface ProgressListener {
-    fun onMenuClicked(view: View, downloadId: Long, isRegular: Boolean)
+    fun onCloseClicked(downloadId: Long, isRegular: Boolean)
+    fun onPlayPauseDownloadClicked(
+        view: View,
+        downloadId: Long,
+        isRegular: Boolean,
+        isPlay: Boolean
+    )
 }
--- app/src/main/java/com/files/video/downloader/videoplayerdownloader/downloader/ui/processing/ProcessingFragment.kt
+++ app/src/main/java/com/files/video/downloader/videoplayerdownloader/downloader/ui/processing/ProcessingFragment.kt
@@ -1,4 +1,93 @@
 package com.files.video.downloader.videoplayerdownloader.downloader.ui.processing
 
-class ProcessingFragment {
+import android.content.Context
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.databinding.Observable
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
+import com.files.video.downloader.videoplayerdownloader.downloader.base.BaseFragment
+import com.files.video.downloader.videoplayerdownloader.downloader.databinding.FragmentProcessingBinding
+import com.files.video.downloader.videoplayerdownloader.downloader.util.AppLogger
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ProcessingFragment : BaseFragment<FragmentProcessingBinding>(), ProgressListener {
+
+    private val progressViewModel: ProgressViewModel by viewModels()
+
+    private lateinit var progressAdapter: ProcessAdapter
+
+    override fun getViewBinding(): FragmentProcessingBinding {
+        return FragmentProcessingBinding.inflate(layoutInflater)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        progressAdapter = ProcessAdapter(emptyList(), this)
+
+        progressViewModel.progressInfos.addOnPropertyChangedCallback(object :
+            Observable.OnPropertyChangedCallback() {
+            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
+                val manager =
+                    WrapContentLinearLayoutManager(
+                        requireContext(),
+                        LinearLayoutManager.VERTICAL,
+                        false
+                    )
+                binding.rcvProcess.layoutManager = manager
+                binding.rcvProcess.adapter = progressAdapter
+                progressViewModel.progressInfos.get()?.let { progressAdapter.setData(it) }
+            }
+
+        })
+
+    }
+
+    override fun onCloseClicked(downloadId: Long, isRegular: Boolean) {
+        progressViewModel.cancelDownload(downloadId, true)
+    }
+
+    override fun onPlayPauseDownloadClicked(
+        view: View,
+        downloadId: Long,
+        isRegular: Boolean,
+        isPlay: Boolean
+    ) {
+        if (isPlay) {
+            progressViewModel.pauseDownload(downloadId)
+        } else {
+            progressViewModel.resumeDownload(downloadId)
+        }
+    }
+}
+
+class WrapContentLinearLayoutManager : LinearLayoutManager {
+    constructor(context: Context?) : super(context) {}
+    constructor(context: Context?, orientation: Int, reverseLayout: Boolean) : super(
+        context,
+        orientation,
+        reverseLayout
+    ) {
+    }
+
+    constructor(
+        context: Context?,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(context, attrs, defStyleAttr, defStyleRes) {
+    }
+
+    override fun onLayoutChildren(recycler: Recycler, state: RecyclerView.State) {
+        try {
+            super.onLayoutChildren(recycler, state)
+        } catch (e: IndexOutOfBoundsException) {
+            AppLogger.e("meet a IOOBE in RecyclerView")
+        }
+    }
 }
